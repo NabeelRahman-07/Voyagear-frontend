73d2b37bda047fc9b53a2eed0cf0d2e96410cf5d
NabeelRahman-07 <nabeelrahman246@gmail.com>
2026-01-21 23:31:23 +0530

    axios timeout

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
   timeout: 15000,
 });
 
-export default api
+export default api;
