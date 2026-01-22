c2c2f11ae6db0cf05de5eaa58f69cb2ca15d670a
NabeelRahman-07 <nabeelrahman246@gmail.com>
2026-01-22 10:43:01 +0530

    products

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-alert("AXIOS INSTANCE LOADED");
-
 const api = axios.create({
   baseURL: "https://e-com-backend-qhgo.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
+  timeout: 15000,
 });
 
 export default api;
